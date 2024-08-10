015b8f496775ec64665aa5c3e2bba6af44d9c216
Radhika Patel <67474415+Radhikapatel-code@users.noreply.github.com>
2024-08-10 16:59:37 +0530

    Add files via upload

--- 404.js
+++ 404.js
@@ -1,41 +1,42 @@
 import React from 'react'
 import Head from 'next/head'
 
-const NotFound1 = (props) => {
+const NotFound = (props) => {
   return (
     <>
-      <div className="not-found1-container">
+      <div className="not-found-container">
         <Head>
-          <title>Spotless Hungry Crocodile</title>
+          <title>404 - Not Found</title>
         </Head>
         <h3>OOPS! PAGE NOT FOUND</h3>
-        <div className="not-found1-container1">
-          <h1 className="not-found1-text1">404</h1>
+        <div className="not-found-container1">
+          <h1 className="not-found-text1">404</h1>
         </div>
-        <div className="not-found1-container2">
-          <h2 className="not-found1-text2">
+        <div className="not-found-container2">
+          <h2 className="not-found-text2">
             WE ARE SORRY, BUT THE PAGE YOU REQUESTED WAS NOT FOUND
           </h2>
         </div>
       </div>
       <style jsx>
         {`
-          .not-found1-container {
+          .not-found-container {
             width: 100%;
             display: flex;
+            overflow: auto;
             min-height: 100vh;
             align-items: center;
             flex-direction: column;
             justify-content: center;
           }
-          .not-found1-container1 {
+          .not-found-container1 {
             display: flex;
             position: relative;
             align-items: center;
             flex-direction: column;
             justify-content: center;
           }
-          .not-found1-text1 {
+          .not-found-text1 {
             color: rgb(38, 38, 38);
             font-size: 252px;
             margin-top: -20px;
@@ -43,14 +44,14 @@ const NotFound1 = (props) => {
             margin-bottom: -20px;
             letter-spacing: -20px;
           }
-          .not-found1-container2 {
+          .not-found-container2 {
             width: 421px;
             display: flex;
             align-items: center;
             flex-direction: column;
             justify-content: center;
           }
-          .not-found1-text2 {
+          .not-found-text2 {
             text-align: center;
             font-weight: 400;
           }
@@ -60,4 +61,4 @@ const NotFound1 = (props) => {
   )
 }
 
-export default NotFound1
+export default NotFound
--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom'
+import Head from 'next/head'
 
-import './style.css'
-import NotFound1, { NotFound } from './views/not-found'
-import Home from './views/home'
+import Navbar from '../components/navbar'
+import Hero from '../components/hero'
+import Features1 from '../components/features1'
+import CTA from '../components/cta'
+import Features2 from '../components/features2'
+import Pricing from '../components/pricing'
+import Steps from '../components/steps'
+import Testimonial from '../components/testimonial'
+import Contact from '../components/contact'
+import Footer from '../components/footer'
 
-const App = () => {
+const Home = (props) => {
   return (
-    <Router>
-      <Switch>
-        <Route component={NotFound1} exact path="/not-found" />
-        <Route component={Home} exact path="/" />
-        <Route component={NotFound} path="**" />
-        <Redirect to="**" />
-      </Switch>
-    </Router>
+    <>
+      <div className="home-container">
+        <Head>
+          <title>Spotless Hungry Crocodile</title>
+        </Head>
+        <Navbar></Navbar>
+        <Hero></Hero>
+        <Features1></Features1>
+        <CTA></CTA>
+        <Features2></Features2>
+        <Pricing></Pricing>
+        <Steps></Steps>
+        <Testimonial></Testimonial>
+        <Contact></Contact>
+        <Footer></Footer>
+      </div>
+      <style jsx>
+        {`
+          .home-container {
+            width: 100%;
+            display: flex;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+export default Home
